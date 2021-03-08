d1dc3f4b51b6068f17770e225a23015985900613
luciaberon <luciaberonwaingart@hotmail.com>
2021-03-08 18:38:53 -0300

    Logo added

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ newBookBtn.addEventListener('click', showForm);
 closeBtn = document.querySelector("#close");
 btn.addEventListener('click',addBookToLibrary);
 closeBtn.addEventListener('click',showForm);
+const warning = document.querySelector("#warning");
 
 class Book {
   constructor(author,title,pages,read) {
@@ -30,7 +31,12 @@ function addBookToLibrary() {
   const isRead = read.checked;
 
   let book = new Book(newAuthor,newName,newPage,isRead);
-  console.log(book);
+  const validate = validateForm(book);
+  if (validate.length > 0) {
+    addWarnings(validate);
+    return "failed";
+  }
+
   myLibrary.push(book);
   updateLibrary();
   showForm();
@@ -50,8 +56,10 @@ function updateLibrary() {
     const pages = document.createElement('li');
     const read = document.createElement('li');
 
-    author.textContent = myLibrary[i].author;
-    pages.textContent = myLibrary[i].pages;
+
+
+    author.textContent = `Author: ${myLibrary[i].author}`;
+    pages.textContent = `Pages: ${myLibrary[i].pages}`;
     read.textContent = myLibrary[i].read;
 
     list.appendChild(author);
@@ -59,7 +67,8 @@ function updateLibrary() {
     list.appendChild(read);
     
     console.log("library author:",myLibrary[i].author);
-    h2.textContent = myLibrary[i].author;
+    h2.textContent = myLibrary[i].title;
+    h2.style.letterSpacing = "3px";
     book.appendChild(h2);
     book.appendChild(list);
     library.appendChild(book);
@@ -69,6 +78,36 @@ function updateLibrary() {
 
 function showForm() {
   form.classList.toggle("active");
+  document.body.classList.toggle("blur");
   form.classList.toggle("closed");
   
+}
+
+
+function validateForm(form) {
+  const warnings = [];
+  if (form.author == "") { 
+    warnings.push("Book must have an author.");
+  }
+
+  if (form.title == "") {
+    warnings.push("Book must have a title.")
+  }
+
+  if (form.pages == "") {
+    warnings.push("Book must have number of pages.")
+  }
+
+  return warnings;
+
+}
+
+function addWarnings(warnings) {
+  warning.innerHTML = "";
+  for (let i = 0; i<warnings.length; i++) {
+    console.log("WARNING: ",warnings[i]);
+    let w = document.createElement("li");
+    w.textContent += warnings[i];
+    warning.appendChild(w);
+  }
 }
