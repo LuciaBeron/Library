23cb5d686f3b153502ce6096c2d45911fc56ff47
luciaberon <luciaberonwaingart@hotmail.com>
2021-03-09 18:33:06 -0300

    Remove book feature added

--- js/script.js
+++ js/script.js
@@ -48,7 +48,11 @@ function updateLibrary() {
   for (let i = 0; i<myLibrary.length; i++) {
     console.log("OBJECT ABOUT TO BE ADDED",myLibrary[i]);
     const book = document.createElement('article');
+    book.classList.add("book");
     
+    const x = document.createElement('button');
+    x.innerHTML = "&#10005;";
+    x.classList.add('remove');
     const h2 = document.createElement('h2');
 
     const list = document.createElement('ul');
@@ -60,7 +64,12 @@ function updateLibrary() {
 
     author.textContent = `Author: ${myLibrary[i].author}`;
     pages.textContent = `Pages: ${myLibrary[i].pages}`;
-    read.textContent = myLibrary[i].read;
+    
+    const checkBox = setCheckbox(myLibrary[i].read);
+    read.textContent = "Read";
+    read.appendChild(checkBox);
+
+    console.log(setCheckbox(myLibrary[i].read));
 
     list.appendChild(author);
     list.appendChild(pages);
@@ -69,6 +78,8 @@ function updateLibrary() {
     console.log("library author:",myLibrary[i].author);
     h2.textContent = myLibrary[i].title;
     h2.style.letterSpacing = "3px";
+
+    book.appendChild(x);
     book.appendChild(h2);
     book.appendChild(list);
     library.appendChild(book);
@@ -111,3 +122,40 @@ function addWarnings(warnings) {
     warning.appendChild(w);
   }
 }
+
+function setCheckbox(x) {
+  const complete = document.createElement('input');
+  complete.setAttribute('type','checkbox');
+  complete.classList.add('red-check');
+
+  if (x === true) {
+    complete.checked = true;
+
+  } else {
+    complete.checked = false;
+  }
+  return complete;
+}
+
+library.addEventListener('click', function(e) {
+  const btn = e.target;
+  const articles = document.querySelectorAll("article");
+  console.log("ARTICLES: ",articles);
+
+
+  const container = btn.closest("article");
+  if (btn.tagName = "button" && btn.classList.contains('remove')) { /// if the event was triggered by the remove button, proceed...
+    
+    for (let i = 0; i<articles.length; i++) {
+        console.log("ARTICLE: ",articles[i]);
+        const headingOfList = articles[i].querySelector("h2").textContent;
+        const title = container.querySelector("h2").textContent;
+        if (headingOfList == title ) {
+          myLibrary.splice(i,1);
+          updateLibrary();
+        }
+    }        
+  }
+
+})
+
